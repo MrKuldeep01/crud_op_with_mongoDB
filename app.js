a53bfd55febb7f4ab5d5a1f0597e68630e5d81b0
Mr. kuldeep <kkharoliya20@gmail.com>
2024-06-08 00:45:24 +0530

    port related stuff

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+require('dotenv').config()
 const port = process.env.PORT || 3000;
 const userModel = require("./models/user.model");
 app.set("view engine", "ejs");
